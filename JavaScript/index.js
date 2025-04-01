87c89ff25b864e75819f2a421d08b86f4f5658bb
Nikesh Shrestha <nikupiku059@gmail.com>
2025-04-01 16:26:33 +0545

    variable and data type

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,7 +2,6 @@
 //variables
 fullname="nikesh shrestha";
 FullName="Don dai";
-
 let Address ="Sindhupalchok";//let can be updated
 Address="lokanthali";
 var age=19;// var can be updated and re-declared
@@ -13,3 +12,20 @@ const DOB=2003;// const cannot do both(update,re-declare)
 console.log(Address);
 console.log(age);
 console.log(DOB);
+
+//data type    
+const student = {  //Object (colllection os data)
+    fullName:"Nikesh Shrestha", //string
+    age:22, //number
+    GPA:3.9, //number
+    pass:"true",//boolean
+};
+//update in the object
+// const ma object ma xa vanya update hunxa 
+student["age"]= student["age"]-1;
+student["fullName"]= "nikesh Shrestha";
+console.log(student);
+
+const profile ={
+
+}
