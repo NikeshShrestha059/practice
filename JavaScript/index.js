cac46e80fda71d5fc94a027fd541e0b93ebfe117
Nikesh Shrestha <nikupiku059@gmail.com>
2025-04-01 15:50:58 +0545

    JavaScript started

---
+++ JavaScript/index.js
@@ -0,0 +1,15 @@
+// console.log("Nikesh Shrestha");
+//variables
+fullname="nikesh shrestha";
+FullName="Don dai";
+
+let Address ="Sindhupalchok";//let can be updated
+Address="lokanthali";
+var age=19;// var can be updated and re-declared
+var age=20;
+var age=21;
+var age=22;
+const DOB=2003;// const cannot do both(update,re-declare)
+console.log(Address);
+console.log(age);
+console.log(DOB);
