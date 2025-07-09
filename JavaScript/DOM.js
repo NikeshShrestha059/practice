0f9f471f4c81acd28c41eb3c19ae249280db8a21
Nikesh Shrestha <nikupiku059@gmail.com>
2025-07-09 15:52:25 +0545

    project

---
+++ JavaScript/DOM.js
@@ -0,0 +1,21 @@
+// const question = document.createElement('input');
+        // question.type= 'text';
+        // question.placeholder="Question";
+        // root.appendChild(question);
+        
+        
+        // const radio = document.createElement('input');
+        // radio.type = 'radio';
+        // root.appendChild(radio);
+
+        // const label = document.createElement('label');
+        // label.textContent = 'Radio';
+        // root.appendChild(label);
+    
+        // const button1 = document.createElement('button');
+        // button1.textContent = "Option";
+        // root.appendChild(button1);
+
+        // const button = document.createElement('button');
+        // button.textContent = "Submit quiz";
+        // root.appendChild(button);
